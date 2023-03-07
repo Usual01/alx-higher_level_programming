f9fc01027e5f5cf925d2467d6b3a70da5313f454
mike <mikedami001@gmail.com>
2023-03-07 13:58:03 +0000

    mine

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # prints ASCII alphabet in lowercase
-for i in range(97, 122):
+for i in range((97, 122)+1):
 	if i != 101 and i != 113:
-    		print("{}".format(chr(i)))
+    		print("{}".format(chr(i)), end='')
