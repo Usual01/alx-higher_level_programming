118d6a8a3f72563247124abeb7e503b4bbb75af6
mike <mikedami001@gmail.com>
2023-03-07 15:31:47 +0000

    naira

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# isupper: A function that prints strings in upper character
-# str: string to change
-# Returns: capitalized string
-
-
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 65 and ord(c) <= 90:
-        	print("{}".format(c), end="")
-    print("")
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
