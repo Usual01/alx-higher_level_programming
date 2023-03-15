befd4e58fa95b3d79ab50124ab524358dbc69db0
michael <mikedami001@gmail.com>
2023-03-15 16:02:54 -0700

    replaaces all instance of an index

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-
-# function that replaces all occurences of an element by another in a new list
 def search_replace(my_list, search, replace):
-    # create a new list to return
-    new_list = []
-    # loop through the list to identify the search then replace
-    for i in range(len(my_list)):
-        # check if search value is present for the replace to take place
-        if search == my_list[i]:
-            my_list[i] == replace
-            new_list.append(my_list[i])
-        else:
-            new_list.append(my_list[i])
-    return new_list
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
