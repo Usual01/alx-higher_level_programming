9cdb1afdb948f6e1bc237fbfba7c3a0a559d3d4c
michael <mikedami001@gmail.com>
2023-04-11 20:57:36 -0700

    copie instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""chexks a class if a function is present"""
+
+    def is_same_class(obj, a_class):
+        """The methof returns true is a duplicate is found
+        arguments: the object, the copied type/instance (a_class)
+        Returns true if it finds instance otherwise false
+        """
+        if type(obj) == a_class:
+            return True
+        return False
+
