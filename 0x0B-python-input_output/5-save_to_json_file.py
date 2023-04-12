6e8fa22159026a3d41b40cf4d44cea0c89484a7f
michael <mikedami001@gmail.com>
2023-04-12 05:46:39 -0700

    w+

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@
 
     def save_to_json_file(my_obj, filename):
         """ opens and closes using with and Writes an into object to a text file using JSON format"""
-        with open(filename, "w") as f:
+        with open(filename, "w+") as f:
             json.dump(my_obj, f)
