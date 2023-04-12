b1d86c738cd4943d79c65a64dc3aa7c3876a7f18
michael <mikedami001@gmail.com>
2023-04-11 20:04:58 -0700

    list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" list of available attributes and methods of an object"""
+
+def lookup(self):
+    """ list of available attributes and methods of an object"""
+    return (self)
