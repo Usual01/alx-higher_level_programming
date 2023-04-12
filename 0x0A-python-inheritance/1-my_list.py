bdbad4ade4dd5552e23c89b3b8de979fe8ba0e30
michael <mikedami001@gmail.com>
2023-04-11 21:23:48 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ a class that inherits properties from a main class"""
 
-class MyList(list):
-    """has a method that prints out a sorted list"""
 
-    def print_sorted(self):
-        """prints out a sorted list in the increasing order"""
-        print(sorted(self)
+    class MyList(list):
+        """has a method that prints out a sorted list"""
+
+        def print_sorted(self):
+            """prints out a sorted list in the increasing order"""
+            print(sorted(self)
 
