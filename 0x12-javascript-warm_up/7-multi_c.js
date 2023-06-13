dc78f050ebc883390a378115752ce1ecc32fa309
michael <mikedami001@gmail.com>
2023-06-13 10:23:20 -0700

    ls

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = process.argv[2];
+
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let a = 0; a < x; a++) {
+    console.log('C is fun');
+  }
+}
