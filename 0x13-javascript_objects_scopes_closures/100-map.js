d2b7a5010d6f383e66c4a7eea92ab9a8809295f4
michael <mikedami001@gmail.com>
2023-08-30 06:12:15 -0700

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+
+const { list } = require('./100-data.js');
+
+const newList = list.map((x, index) => x * index);
+
 console.log(list);
-console.log(list.map((x, index) => x * index));
+console.log(newList);
